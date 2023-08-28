906653fad35eb2d05d46cee69d1028ccae81c632
MONTH4 <goei4559@gmail.com>
2023-08-29 00:02:47 +0900

    [build] spring boot init
    스프링 부트 build

---
+++ 1. services/Gaebugger/src/main/java/com/example/gaebugger/GaebuggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaebugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaebuggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaebuggerApplication.class, args);
+	}
+
+}
