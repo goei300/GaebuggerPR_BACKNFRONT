e6bf9f184e63e56018935f78642c9bf8959139ac
MONTH4 <goei4559@gmail.com>
2023-09-10 16:29:38 +0900

    [feat] service 페이지 구현

--- 1. services/Gaebugger/src/main/frontend/src/App.js
+++ 1. services/Gaebugger/src/main/frontend/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Service from './pages/Services/Service';
 import About from './pages/About/About';
 
 function App() {
+/*    const [message, setMessage] = useState([]);
+    useEffect(() => {
+        fetch("/hello")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setMessage(data);
+            });
+    }, []);*/
   return (
       <Router>
         <div>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Service />} />
                 <Route path="/about" element={<About />} />
             </Routes>
 
