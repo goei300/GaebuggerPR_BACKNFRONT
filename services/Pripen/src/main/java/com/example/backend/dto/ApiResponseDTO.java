995db081eb610a3f772fa3319e40f31f02c5362f
MONTH4 <goei4559@gmail.com>
2023-11-09 10:33:10 +0900

    [feat] backup
    before 해커톤

--- services/Pripen/src/main/java/com/example/backend/dto/ApiResponseDTO.java
+++ services/Pripen/src/main/java/com/example/backend/dto/ApiResponseDTO.java
@@ -14,11 +14,10 @@ public class ApiResponseDTO {
     private int process_Law_Violate;
     private int process_Law_Danger;
     private int process_Guide_Violate;
-    private int process_Omission_Paragraph;
+    private int process_Omission_Paragraph;     // 이건 기재 항목 누락 몇 건인지
     private int process_Score;
     private List<Paragraph> process_Paragraph;
     private List<Issue> process_Issues;
-    private String process_Modified;
 
 
     @Data
@@ -38,13 +37,12 @@ public class ApiResponseDTO {
         private int issue_id;
         private int issue_paragraph_id;
         private String issue_type;
-        private int issue_score;
+        private int issue_score;    // 이거 추가됨. issue 감점 점수 (양수로 표시 ex) -3,-5,-15, -51(이건 기재항목 누락))
         private String issue_content;
         private String issue_reason;
         private int issue_startIndex;
         private int issue_endIndex;
-        private int issue_case;
+        private int issue_case;         // 이거 추가됨. issue_case (이건 bp에 보여주려는건데 22개 유형중 어떤 유형의 위반 인지? 2~22사이의 int형 )
         private List<String> issue_guideline;
-        private String issue_goodCase;
     }
 }
