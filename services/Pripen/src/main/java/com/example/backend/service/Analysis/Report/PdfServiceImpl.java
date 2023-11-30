73a8214a2e9d5ad8232b78771047ebf4c78b00f1
MONTH4 <goei4559@gmail.com>
2023-11-30 14:16:57 +0900

    [feat] createPdf 표지, 파일 정렬, 한국어 폰트 구현
    표지와 파일 정렬하여 순서에 맞게 나열함.
    기존에 영어폰트만 적용되서 별도로 한국어 폰트 가져온 후 적용함.

--- services/Pripen/src/main/java/com/example/backend/service/Analysis/Report/PdfServiceImpl.java
+++ services/Pripen/src/main/java/com/example/backend/service/Analysis/Report/PdfServiceImpl.java
@@ -1,5 +1,12 @@
 package com.example.backend.service.Analysis.Report;
 
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.io.font.FontProgram;
+import com.itextpdf.io.font.FontProgramFactory;
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.layout.element.Paragraph;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import com.itextpdf.io.image.ImageData;
@@ -12,7 +19,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PdfServiceImpl implements PdfService {
@@ -31,8 +40,15 @@ public class PdfServiceImpl implements PdfService {
         PdfWriter writer = new PdfWriter(pdfFilePath);
         PdfDocument pdf = new PdfDocument(writer);
         Document document = new Document(pdf);
+        // 표지 추가
+        addCoverPage(document);
 
-        for (MultipartFile file : files) {
+        // 파일 이름에 따라 정렬
+        List<MultipartFile> sortedFiles = files.stream()
+                .sorted(Comparator.comparing(MultipartFile::getOriginalFilename))
+                .collect(Collectors.toList());
+
+        for (MultipartFile file : sortedFiles) {
             byte[] bytes = file.getBytes();
             ImageData imageData = ImageDataFactory.create(bytes);
             Image image = new Image(imageData);
@@ -41,5 +57,24 @@ public class PdfServiceImpl implements PdfService {
 
         document.close();
         return pdfFilePath;
+    }
+
+    private void addCoverPage(Document document) throws IOException {
+        // 한글 폰트 파일 경로
+        String fontPath = "./fonts/NotoSansKR-Bold.ttf";
+
+        // PDF 폰트 생성
+        PdfFont koreanFont = PdfFontFactory.createFont(fontPath, PdfEncodings.IDENTITY_H, true);
+
+        // 텍스트에 한글 폰트 적용
+        Paragraph title = new Paragraph("제목").setFont(koreanFont).setFontSize(24).setBold();
+        Paragraph subtitle = new Paragraph("부제목").setFont(koreanFont).setFontSize(18);
+
+        document.add(title);
+        document.add(subtitle);
+    }
+
+    private void addTableOfContents(Document document) {
+        // 목차 구현...
     }
 }
